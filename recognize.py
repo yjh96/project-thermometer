8f042f8290f0f60518aad6747da3c10f97ced8bc
Yoo JaeHyung <11yjh11@naver.com>
2021-01-17 20:24:08 +0900

    Detect Face - Dummy test
    1. target을 빨간 사각형으로 표기하도록 함.
    2. Thermo_data Dummy  데이터를 통해 원하는 좌표의 데이터가 출력되는지 확인.
    3. 마스크를 착용하는 경우, 이마근처의 온도를 측정하기 위해 target을 이마로 조절.
    ========
    온도측정 target 개선을 위해서 얼굴 중앙에서 이마로 target을 조절함
    
    issue : 여러 사람의 위치를 구분하지 못함

--- recognize.py
+++ recognize.py
@@ -1,43 +1,55 @@
 from __future__ import print_function
 import cv2 as cv
-import math
-
 
 def detect(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.equalizeHist(frame_gray)
-    cv.imshow('grey', frame_gray)
-    cv.imshow('',cv.resize(frame_gray,(64,64),0))
-    
+    frame_gray = cv.equalizeHist(frame_gray) 
     faces = face_cascade.detectMultiScale(frame_gray)
-
     print(len(faces),' face detected')
 
     return faces
 
-def display(frame,faces):
-    print(faces)
+def display(frame,faces,thermo_data=None):
 
     for i in range(1,8):
             frame = cv.line(frame,(0,(40*i)),(320,(40*i)),(255,255,255))
             frame = cv.line(frame,((40*i),0),((40*i),320),(255,255,255))
     
     for (x,y,w,h) in faces:  
-        # center = (x + w//2, y + h//2)
+        
+        forehead_1 = (x + w//3, y + 0)
+        forehead_2 = (x + 2*(w//3), y + 1*(h//3))
+
         p1 = (x,y)
         p2 = (x+w,y+h)
-        # frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        frame = cv.rectangle(frame,forehead_1,forehead_2,(0,0,255),thickness=2)
         frame = cv.rectangle(frame,p1,p2,(255,255,255),thickness=2)
-        # faceROI = frame_gray[y:y+h,x:x+w]
-        # cv.imshow('ROI',faceROI)
+
+
         
+     
     cv.imshow('Capture - Face detection', frame)
-    
-def sampling (faces):
-    for (x,y,w,h) in faces:
+
+#def collectiROI(frame):
+    # gathering ROI Data ( identify face )
+    # faceROI = frame_gray[y:y+h,x:x+w]
+    # cv.imshow('ROI',faceROI)
+         
+def targeting (faces):
+    for(x,y,w,h) in faces:
         target = (round((x + w/2)/40), round((y + h/2)/40))
         print('x : ',target[0],' | y : ',target[1])
-        
+        return target
+
+def get_thermo(target,thermo_data):
+
+    print('getthermo',target)
+    print(type(target))
+    
+    if type(target) == tuple:
+        print('thermo data  :', thermo_data[int(target[0]-1)][int(target[1]-1)])
+    else:
+        pass
 
 
 face_cascade = cv.CascadeClassifier()
@@ -52,6 +64,8 @@ cap.set(3, 320)
 cap.set(4, 320)
 cap.set(5, 30)
 
+thermo_data = [[1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8],[2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8],[3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8],[4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8],[5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8],[6.1,6.2,6.3,6.4,6.5,6.6,6.7,6.8],[7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8],[8.1,8.2,8.3,8.4,8.5,8.6,8.7,8.8]]
+
 
 if not cap.isOpened:
     print("error - camera")
@@ -63,9 +77,106 @@ while True:
         print("error - no frame")
         break
 
+    # get information
     faces = detect(frame)
+    target = targeting(faces)
+    get_thermo(target,thermo_data)    
+    
+
+    # display realtime video
     display(frame,faces)
-    sampling(faces)
+
+    ## imshow 필수
+
+    if cv.waitKey(10)==27:
+        break
+from __future__ import print_function
+import cv2 as cv
+
+def detect(frame):
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray) 
+    faces = face_cascade.detectMultiScale(frame_gray)
+    print(len(faces),' face detected')
+
+    return faces
+
+def display(frame,faces,thermo_data=None):
+
+    for i in range(1,8):
+            frame = cv.line(frame,(0,(40*i)),(320,(40*i)),(255,255,255))
+            frame = cv.line(frame,((40*i),0),((40*i),320),(255,255,255))
+    
+    for (x,y,w,h) in faces:  
+        
+        forehead_1 = (x + w//3, y + 0)
+        forehead_2 = (x + 2*(w//3), y + 1*(h//3))
+
+        p1 = (x,y)
+        p2 = (x+w,y+h)
+        frame = cv.rectangle(frame,forehead_1,forehead_2,(0,0,255),thickness=2)
+        frame = cv.rectangle(frame,p1,p2,(255,255,255),thickness=2)
+
+
+        
+     
+    cv.imshow('Capture - Face detection', frame)
+
+#def collectiROI(frame):
+    # gathering ROI Data ( identify face )
+    # faceROI = frame_gray[y:y+h,x:x+w]
+    # cv.imshow('ROI',faceROI)
+         
+def targeting (faces):
+    for(x,y,w,h) in faces:
+        target = (round((x + w/2)/40), round((y + h/2)/40))
+        print('x : ',target[0],' | y : ',target[1])
+        return target
+
+def get_thermo(target,thermo_data):
+
+    print('getthermo',target)
+    print(type(target))
+    
+    if type(target) == tuple:
+        print('thermo data  :', thermo_data[int(target[0]-1)][int(target[1]-1)])
+    else:
+        pass
+
+
+face_cascade = cv.CascadeClassifier()
+
+if not face_cascade.load(cv.samples.findFile('./face.xml')):
+    print("error - face weight")
+    exit(-2)
+
+
+cap = cv.VideoCapture(-1)
+cap.set(3, 320)
+cap.set(4, 320)
+cap.set(5, 30)
+
+thermo_data = [[1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8],[2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8],[3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8],[4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8],[5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8],[6.1,6.2,6.3,6.4,6.5,6.6,6.7,6.8],[7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8],[8.1,8.2,8.3,8.4,8.5,8.6,8.7,8.8]]
+
+
+if not cap.isOpened:
+    print("error - camera")
+    exit(-1)
+
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        print("error - no frame")
+        break
+
+    # get information
+    faces = detect(frame)
+    target = targeting(faces)
+    get_thermo(target,thermo_data)    
+    
+
+    # display realtime video
+    display(frame,faces)
 
     ## imshow 필수
 
