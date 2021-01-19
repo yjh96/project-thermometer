3f3c4c5b8c2c326905b1d8580571b9584063c64e
Yoo JaeHyung <11yjh11@naver.com>
2021-01-19 23:32:18 +0900

    Detect Face - Dummy test 3
    1. 35.5℃ 이하, 37.5℃ 이상 화면 알림문구 추가
    2. 실제 온도와 비슷한 Dummy data를 통한 작동 확인

--- README.md
+++ README.md
@@ -14,9 +14,15 @@
     ```
 
 -   프로그램 개요
+
     ```
     1. Camera를 통한 얼굴 인식 [ Opencv - haar cascade]
     2. 얼굴의 위치를 열화상센서 배열과 대응대도록 연산
     3. 얼굴 위치의 [i,j] 와 대응되는 Sensor값 읽기
-    4. 마스크 착용 여부 인식
+    4. 마스크 착용 여부 표시
+    5. 35.5℃ 미만, 37.5℃ 초과 시 추가 안내 알림 표시
     ```
+
+-   Reference
+
+    <a href = 'https://github.com/ageitgey/face_recognition'> Face recognition
--- recognize.py
+++ recognize.py
@@ -2,6 +2,16 @@ from __future__ import print_function
 import cv2 as cv
 import numpy as np
 
+# import face_recognition as fr
+
+
+
+
+camsize = 320
+scale = int(camsize/8)
+DB= []
+
+
 def detect(frame):
     
     data = []
@@ -20,16 +30,28 @@ def detect(frame):
         
         # 입을 인식해 마스크 착용 여부 확인 ( 1 = 착용,  0 = 미착용 )
         mask = mouth_cascade.detectMultiScale(maskROI)
-        print(type(mask))
         if type(mask) is tuple:
             mask = 1
         else:
             mask = 0
+
         data.append([x,y,w,h,mask])
-        print(mask)
+        
+        # 얼굴 인식 -- 성능문제로 제외
+        # 관심부분 선택
+        # faceROI = frame_gray[y:y+h,x:x+w]
+        # faceROI = cv.cvtColor(faceROI,cv.COLOR_BGR2RGB)
+        # loc = fr.face_locations(faceROI)
+        # encodes = fr.face_encodings(faceROI,loc)
 
-    # cv.imshow('mouth',frame)    
+        # for encode in encodes:
+        #     match = fr.compare_faces(DB,encode)
+        #     name = 'idk'
 
+        #     print(name)
+
+        #     data.append([x,y,w,h,mask])
+            
     data = np.array(data)
 
     return data
@@ -47,10 +69,13 @@ def get_data(faces,thermo_data):
     data = []
 
     for(x,y,w,h,mask) in faces:
+    # for(x,y,w,h,mask,name) in faces:
         # 이마 위치를 target으로 설정
-        target = [round((x+ w//2)/40), round((y + h//6)/40)]
-        thermo = thermo_data[int(target[0]-1)][int(target[1]-1)]
+        target = [round((int(x)+ int(w)//2)/scale), round((int(y) + int(h)//6)/scale)]
+        thermo = thermo_data[int(target[1]-1)][int(target[0]-1)]
+        
         data.append([x,y,w,h,mask,thermo])
+        # data.append([x,y,w,h,mask,name,thermo])
     
     data = np.array(data)
     
@@ -63,10 +88,11 @@ def display(frame,data):
 
     #  8*8 격자
     for i in range(1,8):
-            frame = cv.line(frame,(0,(40*i)),(320,(40*i)),(255,255,255))
-            frame = cv.line(frame,((40*i),0),((40*i),320),(255,255,255))
+            frame = cv.line(frame,(0,(scale*i)),(camsize,(scale*i)),(255,255,255))
+            frame = cv.line(frame,((scale*i),0),((scale*i),camsize),(255,255,255))
     
     for (x,y,w,h,mask,thermo) in data:  
+    # for (x,y,w,h,mask,name,thermo) in data:  
         
         # 이마 위치 표시
         # forehead_1 = (int(x + w//3),int(y + 0))
@@ -75,22 +101,27 @@ def display(frame,data):
 
         p1 = (int(x),int(y))
         p2 = (int(x+w),int(y+h))
+        p3 = (int(x),int(y+h))
 
-        print(mask)
+        
 
         if mask == 0 :
             frame = cv.rectangle(frame,p1,p2,(0,0,255),thickness=3)
-
-
             frame = cv.putText(frame,'No Mask',p1,0,1,(255,255,255),thickness=2)
+            # frame = cv.putText(frame,str(name),p2,0,1,(255,255,255),thickness=2)
 
-        else :
+        else:
             frame = cv.rectangle(frame,p1,p2,(255,255,255),thickness=2)
-
-
-            frame = cv.putText(frame,str(thermo),p1,0,1,(255,255,255),thickness=2)
+            frame = cv.putText(frame,str(thermo),p3,0,1,(255,255,255),thickness=2)
+            # frame = cv.putText(frame,str(name),p2,0,1,(255,255,255),thickness=2)
+            if (float(thermo) >= 37.5 or float(thermo) <= 35.5) :
+                frame = cv.putText(frame,"!!Need Check!!",p1,0,1,(0,0,255),thickness=2)
         
     cv.imshow('Capture - Face detection', frame)
+
+
+
+
 
 face_cascade = cv.CascadeClassifier()
 mouth_cascade = cv.CascadeClassifier()
@@ -104,12 +135,21 @@ if not mouth_cascade.load(cv.samples.findFile('./mouth.xml')):
     exit(-2)
 
 cap = cv.VideoCapture(-1)
-cap.set(3, 320)
-cap.set(4, 320)
+
+cap.set(3, camsize)
+cap.set(4, camsize)
 cap.set(5, 30)
 
 thermo_data = [[1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8],[2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8],[3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8],[4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8],[5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8],[6.1,6.2,6.3,6.4,6.5,6.6,6.7,6.8],[7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8],[8.1,8.2,8.3,8.4,8.5,8.6,8.7,8.8]]
-
+# thermo_data = [[37.7,34.7,34,34.1,37.8,37.2,36.3,37.4],
+# [36.5,36.5,36.5,36.5,36.5,36.5,36.5,36.5],
+# [36.5,36.5,36.5,36.5,36.5,36.5,36.5,36.5],
+# [36.5,36.5,36.5,36.5,36.5,36.5,36.5,36.5],
+# [37.9,37.9,37.9,37.9,37.9,37.9,37.9,37.9],
+# [37.9,37.9,37.9,37.9,37.9,37.9,37.9,37.9],
+# [37.9,37.9,37.9,37.9,37.9,37.9,37.9,37.9],
+# [34.4,34.5,36.4,35,37.8,34.5,36.5,37.7]
+# ]
 
 if not cap.isOpened:
     print("error - camera")
@@ -117,6 +157,7 @@ if not cap.isOpened:
 
 while True:
     ret, frame = cap.read()
+    
     if frame is None:
         print("error - no frame")
         break
